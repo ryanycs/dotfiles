695952fb032ca4c72328ff21469e148f7ea9a771
Ryan.Y.C.Su <Ryan.Y.C.Su@gmail.com>
2024-04-30 22:52:13 +0800

    Add neovim config

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,13 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+local opt = vim.opt
+
+opt.expandtab = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.hlsearch = true
+opt.autoindent = true
+opt.updatetime = 100
+opt.wrap = true
