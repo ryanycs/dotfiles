8c1df9ddfc6cacdf630a64eec291ecb7dad2b16f
Ryan.Y.C.Su <Ryan.Y.C.Su@gmail.com>
2025-07-06 04:53:00 +0800

    feat: disable spell check in nvim

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,6 +2,15 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = false
+  end,
+})
+
 if vim.g.vscode then
   vim.api.nvim_exec2(
     [[
